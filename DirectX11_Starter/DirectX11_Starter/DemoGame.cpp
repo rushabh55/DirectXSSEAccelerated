86d45a037af35f5b96cbfccae374751198902407
Rushabh Gosar <rushabh.techie@gmail.com>
2014-09-15 09:38:02 -0400

    no message
    Signed-off-by: Rushabh Gosar <rushabh.techie@gmail.com>

---
+++ DirectX11_Starter/DirectX11_Starter/DemoGame.cpp
@@ -0,0 +1,315 @@
+// ----------------------------------------------------------------------------
+//  A few notes on project settings
+//
+//  - The project is set to use the UNICODE character set
+//    - This was changed in Project Properties > Config Properties > General > Character Set
+//    - This basically adds a "#define UNICODE" to the project
+//
+//  - The include directories were automagically correct, since the DirectX 
+//    headers and libs are part of the windows SDK
+//    - For instance, $(WindowsSDK_IncludePath) is set as a project include 
+//      path by default.  That's where the DirectX headers are located.
+//
+//  - Two libraries had to be manually added to the Linker Input Dependencies
+//    - d3d11.lib
+//    - d3dcompiler.lib
+//    - This was changed in Project Properties > Config Properties > Linker > Input > Additional Dependencies
+//
+//  - The Working Directory was changed to match the actual .exe's 
+//    output directory, since we need to load the shader files at run time
+//    - This was changed in Project Properties > Config Properties > Debugging > Working Directory
+//
+// ----------------------------------------------------------------------------
+
+#include <Windows.h>
+#include <d3dcompiler.h>
+#include "DemoGame.h"
+
+#pragma region Win32 Entry Point (WinMain)
+
+// Win32 Entry Point
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+				   PSTR cmdLine, int showCmd)
+{
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+#endif
+
+	// Make the game, initialize and run
+	DemoGame game(hInstance);
+	
+	if( !game.Init() )
+		return 0;
+	
+	return game.Run();
+}
+
+#pragma endregion
+
+#pragma region Constructor / Destructor
+
+DemoGame::DemoGame(HINSTANCE hInstance) : DXGame(hInstance)
+{
+	// Set up our custom caption and window size
+	windowCaption = L"Demo DX11 Game";
+	windowWidth = 800;
+	windowHeight = 600;
+}
+
+DemoGame::~DemoGame()
+{
+	// Release all of the D3D stuff that's still hanging out
+	ReleaseMacro(vertexBuffer);
+	ReleaseMacro(indexBuffer);
+	ReleaseMacro(vertexShader);
+	ReleaseMacro(pixelShader);
+	ReleaseMacro(vsConstantBuffer);
+	ReleaseMacro(inputLayout);
+}
+
+#pragma endregion
+
+#pragma region Initialization
+
+// Initializes the base class (including the window and D3D),
+// sets up our geometry and loads the shaders (among other things)
+bool DemoGame::Init()
+{
+	if( !DXGame::Init() )
+		return false;
+
+	// Set up buffers and such
+	CreateGeometryBuffers();
+	LoadShadersAndInputLayout();
+
+	// Set up view matrix (camera)
+	// In an actual game, update this when the camera moves (so every frame)
+	XMVECTOR position	= XMVectorSet(0, 0, -5, 0);
+	XMVECTOR target		= XMVectorSet(0, 0, 0, 0);
+	XMVECTOR up			= XMVectorSet(0, 1, 0, 0);
+	XMMATRIX V			= XMMatrixLookAtLH(position, target, up);
+	XMStoreFloat4x4(&viewMatrix, XMMatrixTranspose(V));
+
+	// Set up world matrix
+	// In an actual game, update this when the object moves (so every frame)
+	XMMATRIX W = XMMatrixIdentity();
+	XMStoreFloat4x4(&worldMatrix, XMMatrixTranspose(W));
+
+	return true;
+}
+
+// Creates the vertex and index buffers for a single triangle
+void DemoGame::CreateGeometryBuffers()
+{
+	XMFLOAT4 red	= XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
+	XMFLOAT4 green	= XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f);
+	XMFLOAT4 blue	= XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f);
+
+	// Set up the vertices
+	Vertex vertices[] = 
+	{
+		{ XMFLOAT3(+0.0f, +1.0f, +0.0f), red },
+		{ XMFLOAT3(-1.5f, -1.0f, +0.0f), green },
+		{ XMFLOAT3(+1.5f, -1.0f, +0.0f), blue },
+	};
+
+	// Create the vertex buffer
+	D3D11_BUFFER_DESC vbd;
+    vbd.Usage					= D3D11_USAGE_IMMUTABLE;
+    vbd.ByteWidth				= sizeof(Vertex) * 3; // Number of vertices
+    vbd.BindFlags				= D3D11_BIND_VERTEX_BUFFER;
+    vbd.CPUAccessFlags			= 0;
+    vbd.MiscFlags				= 0;
+	vbd.StructureByteStride		= 0;
+    D3D11_SUBRESOURCE_DATA initialVertexData;
+    initialVertexData.pSysMem	= vertices;
+    HR(device->CreateBuffer(&vbd, &initialVertexData, &vertexBuffer));
+
+	// Set up the indices
+	UINT indices[] = { 0, 2, 1 };
+
+	// Create the index buffer
+	D3D11_BUFFER_DESC ibd;
+    ibd.Usage					= D3D11_USAGE_IMMUTABLE;
+    ibd.ByteWidth				= sizeof(UINT) * 3; // Number of indices
+    ibd.BindFlags				= D3D11_BIND_INDEX_BUFFER;
+    ibd.CPUAccessFlags			= 0;
+    ibd.MiscFlags				= 0;
+	ibd.StructureByteStride		= 0;
+    D3D11_SUBRESOURCE_DATA initialIndexData;
+    initialIndexData.pSysMem	= indices;
+    HR(device->CreateBuffer(&ibd, &initialIndexData, &indexBuffer));
+}
+
+// Loads shaders from compiled shader object (.cso) files, and uses the
+// vertex shader to create an input layout which is needed when sending
+// vertex data to the device
+void DemoGame::LoadShadersAndInputLayout()
+{
+	// Set up the vertex layout description
+	// This has to match the vertex input layout in the vertex shader
+	// We can't set up the input layout yet since we need the actual vert shader
+	D3D11_INPUT_ELEMENT_DESC vertexDesc[] =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT,	0, 0,	D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12,	D3D11_INPUT_PER_VERTEX_DATA, 0}
+	};
+
+	// Load Vertex Shader --------------------------------------
+	ID3DBlob* vsBlob;
+	D3DReadFileToBlob(L"VertexShader.cso", &vsBlob);
+
+	// Create the shader on the device
+	HR(device->CreateVertexShader(
+		vsBlob->GetBufferPointer(),
+		vsBlob->GetBufferSize(),
+		NULL,
+		&vertexShader));
+
+	// Before cleaning up the data, create the input layout
+	HR(device->CreateInputLayout(
+		vertexDesc,
+		ARRAYSIZE(vertexDesc),
+		vsBlob->GetBufferPointer(),
+		vsBlob->GetBufferSize(),
+		&inputLayout));
+
+	// Clean up
+	ReleaseMacro(vsBlob);
+
+	// Load Pixel Shader ---------------------------------------
+	ID3DBlob* psBlob;
+	D3DReadFileToBlob(L"PixelShader.cso", &psBlob);
+
+	// Create the shader on the device
+	HR(device->CreatePixelShader(
+		psBlob->GetBufferPointer(),
+		psBlob->GetBufferSize(),
+		NULL,
+		&pixelShader));
+
+	// Clean up
+	ReleaseMacro(psBlob);
+
+	// Constant buffers ----------------------------------------
+	D3D11_BUFFER_DESC cBufferDesc;
+	cBufferDesc.ByteWidth			= sizeof(vsConstantBufferData);
+	cBufferDesc.Usage				= D3D11_USAGE_DEFAULT;
+	cBufferDesc.BindFlags			= D3D11_BIND_CONSTANT_BUFFER;
+	cBufferDesc.CPUAccessFlags		= 0;
+	cBufferDesc.MiscFlags			= 0;
+	cBufferDesc.StructureByteStride = 0;
+	HR(device->CreateBuffer(
+		&cBufferDesc,
+		NULL,
+		&vsConstantBuffer));
+}
+
+#pragma endregion
+
+#pragma region Window Resizing
+
+// Handles resizing the window and updating our projection matrix to match
+void DemoGame::OnResize()
+{
+	// Handle base-level DX resize stuff
+	DXGame::OnResize();
+
+	// Update our projection matrix since the window size changed
+	XMMATRIX P = XMMatrixPerspectiveFovLH(
+		0.25f * 3.1415926535f,
+		AspectRatio(),
+		0.1f,
+		100.0f);
+	XMStoreFloat4x4(&projectionMatrix, XMMatrixTranspose(P));
+}
+#pragma endregion
+
+#pragma region Game Loop
+
+// Updates the local constant buffer and 
+// push it to the buffer on the device
+void DemoGame::UpdateScene(float dt)
+{
+	// Update local constant buffer data
+	vsConstantBufferData.world		= worldMatrix;
+	vsConstantBufferData.view		= viewMatrix;
+	vsConstantBufferData.projection	= projectionMatrix;
+
+	// Update the constant buffer itself
+	deviceContext->UpdateSubresource(
+		vsConstantBuffer,
+		0,			
+		NULL,		
+		&vsConstantBufferData,
+		0,
+		0);
+}
+
+// Clear the screen, redraw everything, present
+void DemoGame::DrawScene()
+{
+	const float color[4] = {0.4f, 0.6f, 0.75f, 0.0f};
+
+	// Clear the buffer
+	deviceContext->ClearRenderTargetView(renderTargetView, color);
+	deviceContext->ClearDepthStencilView(
+		depthStencilView, 
+		D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL,
+		1.0f,
+		0);
+
+	// Set up the input assembler
+	deviceContext->IASetInputLayout(inputLayout);
+	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	// Set buffers in the input assembler
+	UINT stride = sizeof(Vertex);
+	UINT offset = 0;
+	deviceContext->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
+	deviceContext->IASetIndexBuffer(indexBuffer, DXGI_FORMAT_R32_UINT, 0);
+
+	// Set the current vertex and pixel shaders, as well the constant buffer for the vert shader
+	deviceContext->VSSetShader(vertexShader, NULL, 0);
+	deviceContext->VSSetConstantBuffers(
+		0,	// Corresponds to the constant buffer's register in the vertex shader
+		1, 
+		&vsConstantBuffer);
+	deviceContext->PSSetShader(pixelShader, NULL, 0);
+
+	// Finally do the actual drawing
+	deviceContext->DrawIndexed(
+		3,	// The number of indices we're using in this draw
+		0,
+		0);
+
+	// Present the buffer
+	HR(swapChain->Present(0, 0));
+}
+
+#pragma endregion
+
+#pragma region Mouse Input
+
+// These methods don't do much currently, but can be used for mouse-related input
+
+void DemoGame::OnMouseDown(WPARAM btnState, int x, int y)
+{
+	prevMousePos.x = x;
+	prevMousePos.y = y;
+
+	SetCapture(hMainWnd);
+}
+
+void DemoGame::OnMouseUp(WPARAM btnState, int x, int y)
+{
+	ReleaseCapture();
+}
+
+void DemoGame::OnMouseMove(WPARAM btnState, int x, int y)
+{
+	prevMousePos.x = x;
+	prevMousePos.y = y;
+}
+#pragma endregion
---
+++ DirectX11_Starter/DirectX11_Starter/DemoGame.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <DirectXMath.h>
+#include "DXGame.h"
+
+// Include run-time memory checking in debug builds
+#if defined(DEBUG) || defined(_DEBUG)
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
+
+// For DirectX Math
+using namespace DirectX;
+
+// Vertex struct for triangles
+struct Vertex
+{
+	XMFLOAT3 Position;
+	XMFLOAT4 Color;
+};
+
+// Struct to match vertex shader's constant buffer
+// You update one of these locally, then push it to the corresponding
+// constant buffer on the device when it needs to be updated
+struct VertexShaderConstantBuffer
+{
+	XMFLOAT4X4 world;
+	XMFLOAT4X4 view;
+	XMFLOAT4X4 projection;
+};
+
+// Demo class which extends the base DXGame class
+class DemoGame : public DXGame
+{
+public:
+	DemoGame(HINSTANCE hInstance);
+	~DemoGame();
+
+	// Overrides for base level methods
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	void DrawScene(); 
+
+	// For handing mouse input
+	void OnMouseDown(WPARAM btnState, int x, int y);
+	void OnMouseUp(WPARAM btnState, int x, int y);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+
+private:
+	// Initialization for our "game" demo
+	void CreateGeometryBuffers();
+	void LoadShadersAndInputLayout();
+
+private:
+	// Buffers to hold actual geometry
+	ID3D11Buffer* vertexBuffer;
+	ID3D11Buffer* indexBuffer;
+
+	// Our basic shaders for this example
+	ID3D11PixelShader* pixelShader;
+	ID3D11VertexShader* vertexShader;
+
+	// A few more odds and ends we'll need
+	ID3D11InputLayout* inputLayout;
+	ID3D11Buffer* vsConstantBuffer;
+	VertexShaderConstantBuffer vsConstantBufferData;
+
+	// The matrices to go from 3D model space
+	// to screen space
+	XMFLOAT4X4 worldMatrix;
+	XMFLOAT4X4 viewMatrix;
+	XMFLOAT4X4 projectionMatrix;
+
+	// Keeps track of the old mouse position.  Useful for 
+	// determining how far the mouse moved in a single frame.
+	POINT prevMousePos;
+};
